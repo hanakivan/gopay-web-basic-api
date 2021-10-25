24659ac61b221e9863f7867b4a55bed891c1defe
Ivan hanák <hanakivan@gmail.com>
2021-10-25 09:50:19 +0300

    GoPayWebClient

--- GoPayWebClient.php
+++ GoPayWebClient.php
@@ -20,9 +20,9 @@ class GoPayWebClient {
     }
 
     //https://doc.gopay.com/#payment-creation
-    public function createPayment(float $amount, string $currency, string $orderNumber, string $userName, string $userEmail, string $userCountry)
+    public function createPayment(float $amount, string $currency, string $orderNumber, string $userName, string $userEmail, string $userCountry): int
     {
-        return $this->client->createPayment([
+        $response = $this->client->createPayment([
             "amount" => $amount,
             "currency" => $currency,
             "order_number" => $orderNumber,
@@ -31,17 +31,35 @@ class GoPayWebClient {
                 "goid" => $this->client->getGopay()->getConfig("goid"),
             ],
         ]);
+
+        if($response->hasSucceed()) {
+            return (int)$response->json['3000006529'];
+        } else {
+            throw new GoPayWebClientResponseException((string)$response, $response->statusCode);
+        }
     }
 
     //https://doc.gopay.com/#payment-status
-    public function getPaymentStatus(int $paymentId)
+    public function getPaymentStatus(int $paymentId): string
     {
-        return $this->client->getStatus($paymentId);
+        $response = $this->client->getStatus($paymentId);
+
+        if($response->hasSucceed()) {
+            return (string)$response->json['state'];
+        } else {
+            throw new GoPayWebClientResponseException((string)$response, $response->statusCode);
+        }
     }
 
     //https://doc.gopay.com/#payment-refund
-    public function refundTransaction(int $paymentId, float $amount)
+    public function refundTransaction(int $paymentId, float $amount): bool
     {
-        return $this->client->refundPayment($paymentId, $amount);
+        $response = $this->client->refundPayment($paymentId, $amount);
+
+        if($response->hasSucceed()) {
+            return $response->json['result'] === "FINISHED";
+        } else {
+            throw new GoPayWebClientResponseException((string)$response, $response->statusCode);
+        }
     }
 }
---
+++ GoPayWebClientResponseException.php
@@ -0,0 +1,5 @@
+<?php
+
+class GoPayWebClientResponseException extends Exception {
+
+}
